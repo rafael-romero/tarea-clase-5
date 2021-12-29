11cf137a61b2704ec191c8cc94931272195bc4e9
Rafael <95598903+rafael-romero@users.noreply.github.com>
2021-12-29 16:51:25 -0300

    cambie el tamaño del header poniendolo como un titulo, no estaba apuntando al archivo js correctamente, y la identificacion por clases estaba mal, seria por horas, minutos y segundos para poder acceder a los mismos

--- 5C-reversionado.js
+++ 5C-reversionado.js
@@ -1,8 +1,11 @@
 const $botonCargar = document.querySelector('#boton-cargar')
 $botonCargar.onclick= function(){
-    document.querySelectorAll(' label').value;
-
-    document.querySelector('#resultado-tiempo-total').innerText= //tiempo total videos
+    let datosClaseUno;
+    datosClaseUno=document.querySelectorAll('.clase-uno');
+    for(i=0; i<datosClaseUno.length; i++){
+        console.log(datosClaseUno[i]);
+    }
+    //document.querySelector('#resultado-tiempo-total').innerText= //tiempo total videos
     return false
 }
 
