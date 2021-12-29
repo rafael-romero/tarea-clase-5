191a694353320ab4a1517569b188602a8a39858f
Rafael <95598903+rafael-romero@users.noreply.github.com>
2021-12-29 09:24:55 -0300

    empece a amar la funcion de boton-cargar y coloque placeholder en las primeras 3 casillas de tiempo para que se vea igual al ejemplo que me puso Mauri

---
+++ 5C-reversionado.js
@@ -0,0 +1,8 @@
+const $botonCargar = document.querySelector('#boton-cargar')
+$botonCargar.onclick= function(){
+    document.querySelectorAll(' label').value;
+
+    document.querySelector('#resultado-tiempo-total').innerText= //tiempo total videos
+    return false
+}
+
