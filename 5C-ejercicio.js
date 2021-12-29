702fae657bb133b9a8f64ff93aec4f0cfacb9750
Rafael <95598903+rafael-romero@users.noreply.github.com>
2021-12-29 00:07:27 -0300

    ejercicio ya terminado, desastrozamente no hice commits

--- 5C-ejercicio.js
+++ 5C-ejercicio.js
@@ -2,16 +2,17 @@ let horas=0;
 let minutos=0;
 let segundos=0;
 
-const $botonRegistroTiempoDeUnVideo=document.querySelector('#registroTiempoVideo');
-$botonRegistroTiempoDeUnVideo.onclick=function(horas, minutos, segundos){
-    horas=horas+Number(document.querySelector('#horasVideo').value);
-    minutos=minutos+Number(document.querySelector('#minutosVideo').value);
-    segundos=segundos+Number(document.querySelector('#segundosVideo').value);
-    return (horas, minutos, segundos);
+const $botonRegistroTiempoDeUnVideo=document.querySelector('#registro-tiempo-video');
+$botonRegistroTiempoDeUnVideo.onclick=function(){
+    horas=horas+Number(document.querySelector('#horas-video').value);
+    minutos=minutos+Number(document.querySelector('#minutos-video').value);
+    segundos=segundos+Number(document.querySelector('#segundos-video').value);
+    
+    return false
 }
-
-const $botonCalcularTiempoTotal=document.querySelector('#calcularTiempoTotal');
-$botonCalcularTiempoTotal.onclick=function(horas, minutos, segundos){
+    
+const $botonCalcularTiempoTotal=document.querySelector('#calcular-tiempo-total');
+$botonCalcularTiempoTotal.onclick=function(){
     const segundosPorMinuto=60;
     let minutosAuxiliar=0;
     let horasAuxiliar=0;
@@ -19,7 +20,8 @@ $botonCalcularTiempoTotal.onclick=function(horas, minutos, segundos){
         minutosAuxiliar= Math.trunc(segundos/segundosPorMinuto);
         minutos=minutos+minutosAuxiliar;
         segundos=segundos-(minutosAuxiliar*segundosPorMinuto);
-    }else if (minutos>=segundosPorMinuto){  //para no declarar otra variable con el numero 60 usamos esta misma
+    }
+    if (minutos>=segundosPorMinuto){  //para no declarar otra variable con el numero 60 usamos esta misma
         horasAuxiliar=Math.trunc(minutos/segundosPorMinuto);
         horas=horas+horasAuxiliar;
         minutos=minutos-(horasAuxiliar*segundosPorMinuto);
@@ -28,9 +30,7 @@ $botonCalcularTiempoTotal.onclick=function(horas, minutos, segundos){
     minutos=String(minutos);
     segundos=String(segundos);
 
-    document.querySelector('#tiempoTotal').innerText=`El tiempo total de los videos es ${horas} con ${minutos} y ${segundos}`;
+    document.querySelector('#tiempo-total').textContent=`El tiempo total de los videos es ${horas} con ${minutos} y ${segundos}`;
     return false
 }
-
-
 
