2208fd9f278534c62765a06827ea006e7e054ad5
Rafael <95598903+rafael-romero@users.noreply.github.com>
2021-12-29 22:31:01 -0300

    agregue clases para hs, minutos y segundos para poder agarrar los mismos

--- 5C-reversionado.js
+++ 5C-reversionado.js
@@ -1,10 +1,9 @@
 const $botonCargar = document.querySelector('#boton-cargar')
 $botonCargar.onclick= function(){
     let datosClaseUno;
-    datosClaseUno=document.querySelectorAll('.clase-uno');
-    for(i=0; i<datosClaseUno.length; i++){
-        console.log(datosClaseUno[i]);
-    }
+    datosClaseUno=document.querySelectorAll('.horas');    
+
+
     //document.querySelector('#resultado-tiempo-total').innerText= //tiempo total videos
     return false
 }
