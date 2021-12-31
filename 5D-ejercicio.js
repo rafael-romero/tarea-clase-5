dbf89c58b07fb5a1fe7279140c5de62f5a9fc9a0
Rafael <95598903+rafael-romero@users.noreply.github.com>
2021-12-31 16:08:21 -0300

    se corrigio la palabra length que era por lo cual no funciona, se agrego la funcion calcular numero mas grande

--- 5D-ejercicio.js
+++ 5D-ejercicio.js
@@ -1,6 +1,6 @@
 function armarArregloConNumerosLi(nodeElemento){
     const arreglo=[];
-    for(let i = 0; i<nodeElemento.lenght; i++){
+    for(let i = 0; i<nodeElemento.length; i++){
         arreglo.push(Number(nodeElemento[i].innerText));
     }
     return arreglo
@@ -12,7 +12,6 @@ const $numerosDeListaLi = document.querySelectorAll('li');
 //console.log(numerosDeListaLi[2].innerText);
 const arregloDeNumeros = armarArregloConNumerosLi($numerosDeListaLi);
 
-
 //for(i=0; i<arregloDeNumeros.length; i++){
 //    console.log(arregloDeNumeros[i].innerText);
 //}
@@ -21,18 +20,18 @@ const $botonCalcularPromedio = document.querySelector('#calcular-promedio');
 $botonCalcularPromedio.onclick = function(){
     let sumaTotalNumeros = 0;
     let promedio = 0;
-    for(let i = 0; i<arregloDeNumeros.lenght; i++){
+    for(let i = 0; i<arregloDeNumeros.length; i++){
         sumaTotalNumeros += arregloDeNumeros[i];
     }    
     promedio = sumaTotalNumeros/arregloDeNumeros.length;
-    document.querySelector('#promedio').innerText = String (promedio);
+    document.querySelector('#promedio').innerText = String(promedio);
     return false
 }
 
 const $botonCalcularNumeroMasPequenio = document.querySelector('#calcular-numero-mas-pequenio');
 $botonCalcularNumeroMasPequenio.onclick = function(){
     let numeroMasPequenio = arregloDeNumeros[0];
-    for(let i=1; i<arregloDeNumeros.length; i++){
+    for(let i = 1; i<arregloDeNumeros.length; i++){
         if (arregloDeNumeros[i]<numeroMasPequenio){
             numeroMasPequenio = arregloDeNumeros[i];
         }
@@ -41,6 +40,17 @@ $botonCalcularNumeroMasPequenio.onclick = function(){
     return false
 }
 
+const $botonCalcularNumeroMasGrande = document.querySelector('#calcular-numero-mas-grande');
+$botonCalcularNumeroMasGrande.onclick = function(){
+    let numeroMasGrande = arregloDeNumeros[0];
+    for(let i = 1; i<arregloDeNumeros.length; i++){
+        if(arregloDeNumeros[i]>numeroMasGrande){
+            numeroMasGrande = arregloDeNumeros[i];
+        }
+    }
+    document.querySelector('#numero-mas-grande').innerText = String(numeroMasGrande);
+    return false
+}
 
 
 
